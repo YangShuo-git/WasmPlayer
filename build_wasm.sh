895374b39a39e66ad2e3d198481d58581472d47d
baiyang <17801003317@163.com>
2022-08-16 01:55:55 +0800

    准备工作结束，开始web开发

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,22 +1,21 @@
 #生成.js 和 .wasm文件
 
 export TOTAL_MEMORY=67108864
-export SOURCE_DECODER=./decoder
+export SOURCE_DECODER=./web_client
 export FFMPEG_PATH=./ffmpeg-emcc
 export WASM_PATH=./wasm
 
 export EXPORTED_FUNCTIONS="[ \
-    '_initDecoder'
+    '_initDecoder', \
+    '_GetBuffer', \
+    '_Decode', \
+    '_GetWidth', \
+    '_GetHeight', \
+    '_GetRenderData'
 ]"
 
-# export EXPORTED_FUNCTIONS="[ \
-#     '_openDecoder', \
-#     '_closeDecoder', \
-#     '_decodeData'
-# ]"
-
 echo "Emscripten start..."
-emcc ${SOURCE_DECODER}/decode_test.c \
+emcc ${SOURCE_DECODER}/decode.c\
     ${FFMPEG_PATH}/lib/libavformat.a \
     ${FFMPEG_PATH}/lib/libavcodec.a  \
     ${FFMPEG_PATH}/lib/libswresample.a \
@@ -28,8 +27,7 @@ emcc ${SOURCE_DECODER}/decode_test.c \
     -s TOTAL_MEMORY=${TOTAL_MEMORY} \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s EXPORTED_FUNCTIONS="${EXPORTED_FUNCTIONS}" \
-    -s EXPORTED_RUNTIME_METHODS="[ 'ccall', 'addFunction' ]" \
-    -s RESERVED_FUNCTION_POINTERS=14 \
+    -s EXPORTED_RUNTIME_METHODS="[ 'ccall' ]" \
     -o ${WASM_PATH}/ffmpeg.js
 
 echo "Emscripten end..."
@@ -37,6 +35,4 @@ echo "Emscripten end..."
 cd wasm
 cp ffmpeg.js ffmpeg.wasm ../3rdlibs/nginx-1.22.0/build/html
 
-# cd wasm
-# cp h2642yuv.js h2642yuv.wasm ../3rdlibs/nginx-1.22.0/build/html
 
